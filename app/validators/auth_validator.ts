ae8594593fde562f39129f374efdfed7c29f6db6
Corentin Recanzone <corentindevpro@hotmail.com>
2026-02-15 20:47:24 +0100

    feat: add 2 validator for signin and signup

--- app/validators/auth_validator.ts
+++ app/validators/auth_validator.ts
@@ -4,9 +4,17 @@ import vine from '@vinejs/vine'
  * Sign in validator
  * @type {Object}
  */
-export const signInValidator = vine.compile(
-  vine.object({
-    email: vine.string().email().trim(),
-    password: vine.string().trim(),
-  }),
-)
+export const signInValidator = vine.create({
+  email: vine.string().email().trim(),
+  password: vine.string().trim(),
+})
+
+/**
+ * Sign up validator
+ * @type {Object}
+ */
+export const signUpValidator = vine.create({
+  username: vine.string().trim(),
+  email: vine.string().email().trim(),
+  password: vine.string().trim(),
+})

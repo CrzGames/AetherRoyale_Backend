8766b8e4facb4c4fdd7d4e0dc9901ede1c4f94d0
Corentin Recanzone <corentindevpro@hotmail.com>
2026-02-16 19:28:23 +0100

    feat: add new config s3 for adonisjs

--- adonisrc.ts
+++ adonisrc.ts
@@ -51,6 +51,7 @@ export default defineConfig({
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
     () => import('@adonisjs/auth/auth_provider'),
+    () => import('@adonisjs/drive/drive_provider'),
   ],
 
   /*

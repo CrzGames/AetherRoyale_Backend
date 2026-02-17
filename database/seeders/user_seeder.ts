8de604a989ab55ffded8ea765802e551b80833fa
Corentin Recanzone <corentindevpro@hotmail.com>
2026-02-17 23:42:52 +0100

    feat: add new seeder for model user

---
+++ database/seeders/user_seeder.ts
@@ -0,0 +1,15 @@
+import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import User from '#models/user'
+
+export default class extends BaseSeeder {
+  public async run() {
+    await User.updateOrCreate(
+      { email: 'dev@aetherroyale.com' }, // clé unique pour éviter les doublons
+      {
+        username: 'dev_player',
+        email: 'dev@aetherroyale.com',
+        password: 'password123', // sera hashé auto par le modèle User
+      }
+    )
+  }
+}

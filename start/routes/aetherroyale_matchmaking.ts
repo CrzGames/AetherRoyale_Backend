45a55711f87112e80c0a5640b9eb7b290a53d86b
Corentin Recanzone <corentindevpro@hotmail.com>
2026-02-18 01:56:04 +0100

    feat: add new route for matchmaking cancel match

--- start/routes/aetherroyale_matchmaking.ts
+++ start/routes/aetherroyale_matchmaking.ts
@@ -3,4 +3,8 @@ import { middleware } from '#start/kernel'
 
 const MatchmakingController = () => import('#controllers/matchmaking_controller')
 
+// Boutton "Recherche une partie" côté client du jeu => POST /matchmaking/join
 router.post('/matchmaking/join', [MatchmakingController, 'matchmakingJoin']).use(middleware.auth())
+
+// Boutton "Annuler la recherche de partie" côté client du jeu => POST /matchmaking/leave
+router.post('/matchmaking/cancel', [MatchmakingController, 'matchmakingCancel']).use(middleware.auth())
